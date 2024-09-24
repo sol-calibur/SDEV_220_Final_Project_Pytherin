84a9c67412d776d941c0042ada4a01cc2422ff33
James Klick <jklick@ivytech.edu>
2024-09-24 17:45:54 -0400

    Update models.py
    added Shift Class. Still need to define the function to return the correct values

--- scheduling/models.py
+++ scheduling/models.py
@@ -17,3 +17,24 @@ class Employee(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+class Shift(models.Model):
+    #Times will be in 24hr rather than 12hr (1PM will be listed as 13)
+    #Building runs productivity on Mon-Fri. Does not run prod on Sat or Sun.
+    #Building runs delivery on 'A' shift and 'B' Shift, which provide coverage for Mon-Sat. Does not deliver on Sundays.
+    vacation_start = 0
+    vacation_end = 0
+    prod_day_start = 8
+    prod_day_end = 16
+    prod_night_start = 18
+    prod_night_end = 2
+    delivery_start = 7
+    delivery_end = 18
+    #In case its easier to work with shift length rather than end times
+    vacation_length = 0
+    prod_length = 8
+    delivery_length = 11
+
+    def __str__(self):
+        
+    
